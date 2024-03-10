0d9d85a2a41cecfd5e90be810cb7a245056cece8
Carlos Eduardo A. Ximenes <78832858+CarlosEAXimenes@users.noreply.github.com>
2024-03-10 13:47:07 -0300

    Adicionei o Footer

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    
+<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gradient-to-r from-indigo-800 via-indigo-500 to-indigo-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-white sm:text-center">© 2024 <a href="/app" class="hover:underline">NapierCapital™</a>. 
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Sobre Nós</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Politica de Privacidade</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Contato</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
+</template>
+
